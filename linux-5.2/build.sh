0b5cb6e23c3e81a45550d8eb8357bbe35a1e94fb
Ping <929391874@qq.com>
2019-11-07 22:44:12 +0800

    [ video ] Add video codec config and dts, modify compilation script.

--- linux-5.2/build.sh
+++ linux-5.2/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 if [ $# != 1 ]
 then
 	echo "ERROR,Parameter format:"
@@ -7,51 +9,59 @@ then
 	exit
 fi
 
+BASE=$(pwd)
+OPTION="-C $PWD ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- O=$PWD/out"
 source /ev_toolchain.sh
 
 
 if [ $1 = "all" ]
 then
-	make -j4 Image dtbs ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
-	cp arch/arm64/boot/Image ./
-	cp arch/arm64/boot/dts/allwinner/sun50i-h5-ginpot.dtb ./
+	make -j4 Image dtbs $OPTION || exit
+	cp out/arch/arm64/boot/Image ./out
+	cp out/arch/arm64/boot/dts/allwinner/sun50i-h5-ginpot.dtb ./out
 	exit
 fi
 
 if [ $1 = "dtb" ]
 then
-	make dtbs ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make dtbs $OPTION || exit
 	cp arch/arm64/boot/dts/allwinner/sun50i-h5-ginpot.dtb ./
 	exit
 fi
 
 if [ $1 = "image" ]
 then
-	make Image ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make Image $OPTION || exit
 	cp arch/arm64/boot/Image ./
 	exit
 fi
 
 if [ $1 = "modules" ]
 then
-	make modules ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make modules $OPTION
 	exit
 fi
 
 if [ $1 = "menuconfig" ]
 then
-	make menuconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make menuconfig $OPTION
 	exit
 fi
 
 if [ $1 = "clean" ]
 then
-	make clean ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make clean $OPTION
 	exit
 fi
 
 if [ $1 = "defconfig" ]
 then
-	make ginpot_h5_defconfig ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-
+	make ginpot_h5_defconfig $OPTION
+	exit
+fi
+
+if [ $1 = "mrproper" ]
+then
+	make mrproper $OPTION
 	exit
 fi
